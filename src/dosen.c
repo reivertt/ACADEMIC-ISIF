991df6e02927cb274b44ebb04bca7835850576d0
Nabil Akhtar <billthehuman27@gmail.com>
2022-12-14 23:35:21 +0700

    Added a failsafe and made things pretty

--- src/dosen.c
+++ src/dosen.c
@@ -5,10 +5,10 @@
 
 void input_dosen(){
     system("cls");
-    FILE *fp = fopen("dosen.txt", "r+");
+    FILE *dosenfp = fopen("dosen.txt", "w+");
     struct dosenfile dosen = {"", ""};
 
-    if (fp == NULL){
+    if (dosenfp == NULL){
         printf("File not found\n");
         return;
     }
@@ -17,46 +17,132 @@ void input_dosen(){
     char temp[5];
 
     while(1){
-        printf("Dosenname and NIP Input\n");
-        printf("------------------------\n");
+        system("cls");
+        printf("====================================================\n");
+        printf("|     Student Advisor Registration in Progress     |\n");
+        printf("====================================================\n");
         printf("Dosen NIP: ");
         scanf("\n%s", temp);
-        
-        if (strcmp(temp, "000") == 0){
-            printf("Input cancelled\n");
-            system("cls");
-            break;
-        }
         
         strcpy(dosen.nip, temp);
         printf("Dosen name: ");
         scanf("\n%[^\n]%*c", dosen.dsnname);
 
-        int lim = atoi(dosen.nip);
-        fseek(fp, (lim - 1) * sizeof(struct dosenfile), SEEK_SET);
-        fwrite(&dosen, sizeof(struct dosenfile), 1, fp);
         system("cls");
+        printf("You are about to input data regarding:\n");
+        printf("Advisor's Name: %s\n", dosen.dsnname);
+        printf("NIP: %s\n", dosen.nip);
+
+        printf("continue? (y/n): ");
+        char ttemp;
+        scanf("\n%c", &ttemp);
+        if (ttemp == 'y'){
+            system("cls");
+            printf("Inserted data is as follows:\n");
+            printf("Advisor's Name: %s\n", dosen.dsnname);
+            printf("NIP: %s\n", dosen.nip);
+
+            int lim = atoi(dosen.nip);
+            fseek(dosenfp, (lim - 1) * sizeof(struct dosenfile), SEEK_SET);
+            fwrite(&dosen, sizeof(struct dosenfile), 1, dosenfp);
+
+            printf("Data saved\n");
+        }
+        else{
+            printf("Data not saved\n");
+        }
+
+        printf("Continue inputting data? (y/n): ");
+        scanf("\n%c", &ttemp);
+        if (ttemp == 'n'){
+            printf("Input cancelled\n");
+            printf("Returning to main menu\n");
+            loading();
+            system("cls");
+            break;
+        }
     }
-    fclose(fp);
+    fclose(dosenfp);
 }
 
 void show_dosen(){
-    FILE *fp = fopen("dosen.txt", "r");
+    FILE *dosenfp = fopen("dosen.txt", "r");
     struct dosenfile dosen = {"", ""};
 
-    if (fp == NULL){
+    if (dosenfp == NULL){
         printf("File not found\n");
-        fclose(fp);
+        fclose(dosenfp);
         return;
     }
     
     printf("File found\n");
-    printf("Nama Dosen\tNIP\n");
-    while (!feof(fp)){
-        if (fread(&dosen, sizeof(struct dosenfile), 1, fp) != 0 && strcmp(dosen.dsnname, "") != 0){
+
+    printf("Nama Dosen\tNIP\n"); //todo fix this lmao
+    while (!feof(dosenfp)){
+        if (fread(&dosen, sizeof(struct dosenfile), 1, dosenfp) != 0 && strcmp(dosen.dsnname, "") != 0){
             printf("%s\t\t%s\n", dosen.dsnname, dosen.nip);
         }
     }
     printf("\n");
-    fclose(fp);
+    fclose(dosenfp);
+}
+
+void reset_dosen(){
+    system("cls");
+    FILE *dosenfp = fopen("dosen.txt", "r+");
+    struct dosenfile dosen = {"", ""};
+
+    if (dosenfp == NULL){
+        printf("File not found\n");
+        fclose(dosenfp);
+        return;
+    }
+
+    while(1){
+        system("cls");
+        printf("=============================================\n");
+        printf("|     Advisor Data Deletion in Progress     |\n");
+        printf("=============================================\n");
+        show_dosen();
+
+        printf("Input NIP to delete: ");
+        char temp[5];
+        scanf("\n%s", temp);
+        int lim = atoi(temp);
+        fseek(dosenfp, (lim - 1) * sizeof(struct dosenfile), SEEK_SET);
+
+        if (!fread(&dosen, sizeof(struct dosenfile), 1, dosenfp)){
+            printf("Data not found\n");
+        }
+        else {
+            printf("You are about to delete data regarding:\n");
+            printf("Advisor Name: %s\n", dosen.dsnname);
+            printf("NIP: %s\n", dosen.nip);
+
+            printf("Are you sure? (y/n): ");
+            char sure;
+            scanf("\n%c", &sure);
+            if (sure == 'y'){
+                strcpy(dosen.dsnname, "");
+                strcpy(dosen.nip, "");
+                fwrite(&dosen, sizeof(struct dosenfile), 1, dosenfp);
+                printf("Data has been deleted.");
+            }
+            else{
+                printf("Data has not been deleted.");
+            }
+        }
+        
+        printf(" Do you still wish to delete more data? (y/n): ");
+        char more;
+        scanf("\n%c", &more);
+        if (more == 'n'){
+            printf("Deletion cancelled\n");
+            printf("Returning to main menu");
+            loading();
+            system("cls");
+            break;
+        }
+    }
+    fclose(dosenfp);
 }
--- src/matkul.c
+++ src/matkul.c
@@ -5,10 +5,10 @@
 
 void input_mata_kuliah(){
     system("cls");
-    FILE *mkdat = fopen("mata_kuliah.txt", "w+");
+    FILE *coursefp = fopen("mata_kuliah.txt", "w+");
     struct mapelfile mapel = {"", "", 0, 0, 0, 0, 0};
 
-    if (mkdat == NULL){
+    if (coursefp == NULL){
         printf("File not found\n");
         return;
     }
@@ -17,52 +17,140 @@ void input_mata_kuliah(){
     char temp[5];
 
     while(1){
-        printf("College Subject Input\n");
-        printf("------------------------\n");
-        printf("College Subject Code: ");
+        system("cls");
+        printf("===============================================\n");
+        printf("|     College Course Creation in Progress     |\n");
+        printf("===============================================\n");
+        printf("Course ID: ");
         scanf("\n%s", temp);
         
-        if (strcmp(temp, "000") == 0){
+        strcpy(mapel.mkcode, temp);
+        printf("Course Name: ");
+        scanf("\n%[^\n]%*c", mapel.mkname);
+        printf("Course Credit: ");
+        scanf("\n%d", &mapel.sks);
+
+        system("cls");
+        printf("You are about to input data regarding:\n");
+        printf("Course Name: %s\n", mapel.mkname);
+        printf("Course ID: %s\n", mapel.mkcode);
+        printf("Course Credit: %d\n", mapel.sks);
+
+        printf("continue? (y/n): ");
+        char ttemp;
+        scanf("\n%c", &ttemp);
+        if (ttemp == 'y'){
+            system("cls");
+            printf("Inserted data is as follows:\n");
+            printf("Course Name: %s\n", mapel.mkname);
+            printf("Course ID: %s\n", mapel.mkcode);
+            printf("Course Credit: %d\n", mapel.sks);
+
+            int lim = atoi(mapel.mkcode);
+            fseek(coursefp, (lim - 1) * sizeof(struct mapelfile), SEEK_SET);
+            fwrite(&mapel, sizeof(struct mapelfile), 1, coursefp);
+
+            printf("Data saved\n");
+        }
+        else{
+            printf("Data not saved\n");
+        }
+        printf("Continue inputting data? (y/n): ");
+        scanf("\n%c", &ttemp);
+        if (ttemp == 'n'){
             printf("Input cancelled\n");
+            printf("Returning to main menu\n");
+            loading();
             system("cls");
             break;
         }
-        
-        strcpy(mapel.mkcode, temp);
-        printf("College Subject Name: ");
-        scanf("\n%[^\n]%*c", mapel.mkname);
-
-        int lim = atoi(mapel.mkcode);
-        fseek(mkdat, (lim - 1) * sizeof(struct mapelfile), SEEK_SET);
-        fwrite(&mapel, sizeof(struct mapelfile), 1, mkdat);
-        system("cls");
     }
-    fclose(mkdat);
+    fclose(coursefp);
 }
 
 void show_mata_kuliah(){
     system("cls");
-    FILE *mkdat = fopen("mata_kuliah.txt", "r");
-    if (mkdat == NULL){
+    FILE *coursefp = fopen("mata_kuliah.txt", "r");
+    struct mapelfile mapel = {"", "", 0, 0, 0, 0, 0};
+    
+    if (coursefp == NULL){
         printf("File not found\n");
+        fclose(coursefp);
         return;
     }
     
     printf("File found\n");
-    struct mapelfile mapel = {"", "", 0, 0, 0, 0, 0};
     
     printf("Available College Subjects\n");
     printf("------------------------\n");
     printf("Course Code\nName of the Course\n");
-    while(!feof(mkdat)){
-        if (fread(&mapel, sizeof(struct mapelfile), 1, mkdat) != 0 && strcmp(mapel.mkname, "") != 0){
+    while(!feof(coursefp)){
+        if (fread(&mapel, sizeof(struct mapelfile), 1, coursefp) != 0 && strcmp(mapel.mkname, "") != 0){
             printf("%s\t%s\n", mapel.mkcode, mapel.mkname);
         }
     }
 
-    printf("\n...press any key to continue\n");
     printf("\n");
-    system("pause > null");
+    fclose(coursefp);
+}
+
+void reset_mata_kuliah(){
+    system("cls");
+    FILE *coursefp = fopen("mata_kuliah.txt", "r+");
+    struct mapelfile mapel = {"", "", 0, 0, 0, 0, 0};
     
-    fclose(mkdat);
+    if (coursefp == NULL){
+        printf("File not found\n");
+        fclose(coursefp);
+        return;
+    }
+
+    while(1){
+        system("cls");
+        printf("============================================\n");
+        printf("|     Course Data Deletion in Progress     |\n");
+        printf("============================================\n");
+        show_mata_kuliah();
+
+        printf("Enter the course code to delete: ");
+        char temp[5];
+        scanf("\n%s", temp);
+        int lim = atoi(temp);
+        fseek(coursefp, (lim - 1) * sizeof(struct mapelfile), SEEK_SET);
+        if (!fread(&mapel, sizeof(struct mapelfile), 1, coursefp)){
+            printf("Data not found\n");
+        }
+        else {
+            printf("You are about to delete data regarding:\n");
+            printf("Course Name: %s\n", mapel.mkname);
+            printf("Course ID: %s\n", mapel.mkcode);
+            printf("Course Credit: %d\n", mapel.sks);
+
+            printf("Are you sure? (y/n): ");
+            char ttemp;
+            scanf("\n%c", &ttemp);
+            if (ttemp == 'y'){
+                strcpy(mapel.mkname, "");
+                strcpy(mapel.mkcode, "");
+                mapel.sks = 0;
+                fwrite(&mapel, sizeof(struct mapelfile), 1, coursefp);
+                printf("Data has been deleted.");
+            }
+            else{
+                printf("Data has not been deleted.");
+            }
+        }
+
+        printf(" Do you still wish to delete more data? (y/n): ");
+        char ctemp;
+        scanf("\n%c", &ctemp);
+        if (ctemp == 'n'){
+            printf("Deletion cancelled\n");
+            printf("Returning to main menu\n");
+            loading();
+            system("cls");
+            break;
+        }
+    }   
+    fclose(coursefp);
 }
